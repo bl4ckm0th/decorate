1f2c0cde372f18e95b83a6e1f69ba69228f38be1
m0th <70561659+bl4ckm0th@users.noreply.github.com>
2020-12-01 22:02:20 -0300

    Add files via upload

---
+++ simpletextdecorator.py
@@ -0,0 +1,25 @@
+x = input("Write something! \n")
+def decor(func):
+      def wrap(text):
+          print("\n"+"=*"*(len(text)//2))
+          func(text)
+          print("=*"*(len(text)//2)+"\n")
+      return wrap
+
+def print_text(text):
+      print(text)
+
+print_text = decor(print_text)
+print_text(x)
+y = input("Wanna write again?(y/n)")
+while True:
+    if y == "y":
+       x = input("Write something! \n")
+       print_text(x)
+       y = input("Wanna write again?(y/n)")
+    elif y == "n":
+     break
+    else:
+     print("Only type \"y\" or \"n\"!")
+     y = input("Wanna write again?(y/n)")
+   
